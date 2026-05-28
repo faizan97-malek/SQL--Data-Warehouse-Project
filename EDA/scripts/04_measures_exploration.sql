4a6b0ef6825efcfd6f5e7b60d806ef8c30210e21
Faizanfarid Malek <faizex07@gmail.com>
2026-05-28 11:20:32 -0400

    Enhance measures exploration with key metrics calculations
    Updated the SQL script to include additional metrics calculations such as total sales, total quantity, average price, total orders, total products, and total customers. Removed some previous queries related to order dates and customer ages.

--- EDA/scripts/04_measures_exploration.sql
+++ EDA/scripts/04_measures_exploration.sql
@@ -1,27 +1,47 @@
 /*
 ===============================================================================
-Date Range Exploration 
+Measures Exploration (Key Metrics)
 ===============================================================================
 Purpose:
-    - To determine the temporal boundaries of key data points.
-    - To understand the range of historical data.
+    - To calculate aggregated metrics (e.g., totals, averages) for quick insights.
+    - To identify overall trends or spot anomalies.
 
 SQL Functions Used:
-    - MIN(), MAX(), DATEDIFF()
+    - COUNT(), SUM(), AVG()
 ===============================================================================
 */
 
--- Determine the first and last order date and the total duration in months
-SELECT 
-    MIN(order_date) AS first_order_date,
-    MAX(order_date) AS last_order_date,
-    DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS order_range_months
-FROM gold.fact_sales;
+-- Find the Total Sales
+SELECT SUM(sales_amount) AS total_sales FROM gold.fact_sales
 
--- Find the youngest and oldest customer based on birthdate
-SELECT
-    MIN(birthdate) AS oldest_birthdate,
-    DATEDIFF(YEAR, MIN(birthdate), GETDATE()) AS oldest_age,
-    MAX(birthdate) AS youngest_birthdate,
-    DATEDIFF(YEAR, MAX(birthdate), GETDATE()) AS youngest_age
-FROM gold.dim_customers;
+-- Find how many items are sold
+SELECT SUM(quantity) AS total_quantity FROM gold.fact_sales
+
+-- Find the average selling price
+SELECT AVG(price) AS avg_price FROM gold.fact_sales
+
+-- Find the Total number of Orders
+SELECT COUNT(order_number) AS total_orders FROM gold.fact_sales
+SELECT COUNT(DISTINCT order_number) AS total_orders FROM gold.fact_sales
+
+-- Find the total number of products
+SELECT COUNT(product_name) AS total_products FROM gold.dim_products
+
+-- Find the total number of customers
+SELECT COUNT(customer_key) AS total_customers FROM gold.dim_customers;
+
+-- Find the total number of customers that has placed an order
+SELECT COUNT(DISTINCT customer_key) AS total_customers FROM gold.fact_sales;
+
+-- Generate a Report that shows all key metrics of the business
+SELECT 'Total Sales' AS measure_name, SUM(sales_amount) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Quantity', SUM(quantity) FROM gold.fact_sales
+UNION ALL
+SELECT 'Average Price', AVG(price) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Orders', COUNT(DISTINCT order_number) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Products', COUNT(DISTINCT product_name) FROM gold.dim_products
+UNION ALL
+SELECT 'Total Customers', COUNT(customer_key) FROM gold.dim_customers;
